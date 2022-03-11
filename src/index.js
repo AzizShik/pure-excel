8e0c909aad2197380ececc25e5012ba183f6eaeb
Abdyl-Aziz <azizshihabov@mail.ru>
2022-03-11 16:57:23 +0300

    feat: finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import './module';
+import {Excel} from './component/excel/Excel';
+import {Header} from './component/header/Header';
+import {Formula} from './component/formula/Formula';
+import {Toolbar} from './component/toolbar/Toolbar';
+import {Table} from './component/table/Table';
 import './sass/index.scss';
 
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
 
-console.log('Webpack Start!!!');
-
-async function start() {
-  return await Promise.resolve('async working');
-}
-
-start().then(console.log);
+excel.render();
