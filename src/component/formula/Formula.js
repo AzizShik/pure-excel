bce8d2faf3329043540f70f37d0a1f8837c78113
Abdyl-Aziz <azizshihabov@mail.ru>
2022-04-08 11:28:44 +0300

    feat: add table-logic

--- src/component/formula/Formula.js
+++ src/component/formula/Formula.js
@@ -1,27 +1,45 @@
 import {ExcelComponent} from '../../core/ExcelComponent';
+import {$} from '../../core/dom';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click'],
+      listeners: ['input', 'keydown'],
+      ...options,
     });
   }
 
   toHTML() {
     return `
-            <div class="excel__formula-info">fx</div>
-        <div class="excel__formula-input" contenteditable="true"></div>
+        <div class="excel__formula-info">fx</div>
+        <div id="formula" class="excel__formula-input" 
+        contenteditable="true"></div>
     `;
   }
 
-  onInput(e) {
-    console.log('Formula: onInput', e.target.textContent.trim());
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula');
+    this.$on('table:select', ($cell) => {
+      this.$formula.text($cell.text());
+    });
+    this.$on('table:input', ($cell) => {
+      this.$formula.text($cell.text());
+    });
   }
 
-  onClick() {
+  onInput(e) {
+    this.$emit('formula:input', $(e.target).text());
+  }
 
+  onKeydown(e) {
+    const keys = ['Enter', 'Table'];
+    if (keys.includes(e.key)) {
+      e.preventDefault();
+      this.$emit('formula:done');
+    }
   }
 }
--- src/component/table/Table.js
+++ src/component/table/Table.js
@@ -2,13 +2,17 @@ import {ExcelComponent} from '../../core/ExcelComponent';
 import {createTable} from './table.template';
 import {$} from '../../core/dom';
 import {resizeHandler} from './table.resize.js';
+import {TableSelection} from './TableSelection';
+import {shouldResize, isCell, matrix, nextSelector} from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options,
     });
   }
 
@@ -16,23 +20,66 @@ export class Table extends ExcelComponent {
     return createTable(20);
   }
 
-  onClick() {
-    console.log('click');
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() { 
+    super.init();
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selectCell($cell);
+    this.$on('formula:input', (text) => {
+      this.selection.current.text(text);
+    });
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus();
+    });
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit('table:select', $cell);
   }
 
   onMousedown(e) {
     const el = e.target;
 
-    if (el.dataset.resize) {
+    if (shouldResize(e)) {
       resizeHandler(this.$root, e);
+    } else if (isCell(e)) {
+      const $target = $(e.target);
+      if (e.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`)
+        );
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 
-  onMousemove() {
-    console.log('mousemove');
+  onKeydown(e) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp',
+    ];
+    const id = this.selection.current.id(true);
+    const {key} = e;
+
+    if (keys.includes(key) && !e.shiftKey) {
+      e.preventDefault();
+      const $next = this.$root.find(nextSelector(key, id));
+      this.selectCell($next);
+    }
   }
 
-  onMouseup() {
-    console.log('mouseup');
+  onInput(e) {
+    this.$emit('table:input', $(e.target));
   }
 }
